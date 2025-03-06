7b1bbe62b1eb724a61f3ffa3761ed5e51c13310a
mbalavamsi <mbalavamsi@gmail.com>
2025-03-05 19:01:34 -0500

    Content Update
    Updated the content with attachments

--- src/app/pages/Home/home.tsx
+++ src/app/pages/Home/home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <>
       <Card className='career-summary' title='Let’s Dive Into My Journey!!'>
-        An accomplished Full-Stack Java Developer with over 4 years of specialized knowledge in Banking and Financial Services, Academic, and Retail/E-commerce domains. Expertise includes Agile and Waterfall software development life cycle methodologies, along with proficiency in Java, Spring Boot, AngularJS, and React.js frameworks, AWS, Azure DevOps, Jenkins, and Harness for deployments. I excel in designing scalable microservices architecture, backend and frontend development, database management, and deployment. Demonstrated ability to provide production support, drive enhancements, and resolve complex customer issues. Skilled in collaborating with engineering, product, and customer teams to deliver impactful solutions.
+        An accomplished Full-Stack Java Developer with nearly 4 years of specialized knowledge in Banking and Financial Services, Academic, and Retail/E-commerce domains. Expertise includes Agile and Waterfall software development life cycle methodologies, along with proficiency in Java, Spring Boot, AngularJS, and React.js frameworks, AWS, Azure DevOps, Jenkins, and Harness for deployments. I excel in designing scalable microservices architecture, backend and frontend development, database management, and deployment. Demonstrated ability to provide production support, drive enhancements, and resolve complex customer issues. Skilled in collaborating with engineering, product, and customer teams to deliver impactful solutions.
       </Card>
       <Achievement />
       <ProfessionalExp />
